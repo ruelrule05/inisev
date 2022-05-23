6c5d2a18139d964a386164db56d75657dff1dc08
Ruel Rule <ruel@vernaschediewelt.com>
2022-05-23 14:19:13 +0800

    done with website seeder, model, resource and factory

--- database/seeders/WebsiteSeeder.php
+++ database/seeders/WebsiteSeeder.php
@@ -2,17 +2,18 @@
 
 namespace Database\Seeders;
 
+use App\Models\Website;
 use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
+class WebsiteSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeds.
      *
      * @return void
      */
     public function run()
     {
-        $this->call(WebsiteSeeder::class);
+        Website::factory()->count(10)->create();
     }
 }
